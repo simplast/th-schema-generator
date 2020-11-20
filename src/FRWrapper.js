73c80b8a9bffd9205aaaa80179d9bf35a7d680f5
FateRiddle <fateriddle@gmail.com>
2020-11-20 11:43:25 +0800

    打个补丁，将ui相关props放到schema中

--- src/FRWrapper.js
+++ src/FRWrapper.js
@@ -136,6 +136,15 @@ function Wrapper(
   let displaySchemaString = '';
   try {
     const _schema = idToSchema(flattenWithData, '#', true);
+    if (frProps && frProps.column) {
+      _schema['ui:column'] = frProps.column;
+    }
+    if (frProps && frProps.displayType) {
+      _schema['ui:displayType'] = frProps.displayType;
+    }
+    if (frProps && frProps.showDescIcon) {
+      _schema['ui:showDescIcon'] = frProps.showDescIcon;
+    }
     displaySchema = transformTo({ schema: _schema, ...frProps });
     if (!isNewVersion) {
       displaySchema = newSchemaToOld(displaySchema);
